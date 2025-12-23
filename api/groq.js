6593c8d768ba66f988b97d98be25da58e60ef83d
ashwith-dev <ashwiththatipally@gmail.com>
2025-12-23 23:47:06 +0530

    fix

--- api/groq.js
+++ api/groq.js
@@ -36,7 +36,7 @@ Suggest exactly 15 thoughtful gift ideas.
 
 FORMAT:
 1. Gift Name – short reason
-   Product link
+   Amazon Product link
 
 RULES:
 - Give with the product links
@@ -48,6 +48,9 @@ RULES:
 - Analyse in general world and give the best suggestions
 - Give the links of the products
 - The links should be working , redirect to the product page
+- Use Amazon India links (amazon.in)
+- The product should be given with the company or the brand name with the model
+- Example:https://www.amazon.in/Noise-Wireless-Metallic-Playtime-Instacharge/dp/B0D4QLT8WG/ref=sr_1_1_sspa?crid=2Z1N927ORWHF&dib=eyJ2IjoiMSJ9.mqI-Vl-4XVzBY9LjpBeeZtJ6GdInbsJS6XtEaZrt4yxp3MH7shpvjq_opILbZ2Vue4quO1ygeRqLWZRDOxlCS6FzNS9IOoKrM5OcmHQcM894VPB6h7qT43Xm5FuZmU34XEY0riuaZ9J1WzjQcOMcG3tXjhuMr1nvuK54mdlQwWHlTcOhxtxqnji3szQndxIAW-pgVNG73Fke1ldtccG92DhV_eegf2MQwpNc0YK8vPI.wXc6iP5WU8sAloy_4VXQWaQ5PvlR3CfhX9KY2fZ1ods&dib_tag=se&keywords=noise%2Btws&nsdOptOutParam=true&qid=1766513791&sprefix=noise%2Btw%2Caps%2C486&sr=8-1-spons&aref=lCfhhtpfQ3&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1
 `;
 
     const openaiRes = await fetch(
